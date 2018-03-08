34ab25038ed31867add6d82ee7d1b0a933c3fb73
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-03-07 18:21:05 -0700

    [feature] add some routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App/App';
+import Browse from './App/Browse/Browse';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path='/browse' component={Browse} />
+      <Route path='/' component={App} />
+    </Switch>
+  </Router>,
+document.getElementById('root'));

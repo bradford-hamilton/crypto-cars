d2a6cd78ad7f03f1fac7297396072d9c24d65581
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-03-08 20:45:47 -0700

    start sorting out creation of cars

--- contracts/CryptoCars.sol
+++ contracts/CryptoCars.sol
@@ -1,15 +1,43 @@
 pragma solidity ^0.4.19;
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract CryptoCars is Ownable {
-  string public message;
+  using SafeMath for uint256;
 
-  function CryptoCars() {
-    message = 'get this thing rolling';
+  uint256 carDnaDigits = 16;
+  uint256 carDnaModulus = 10 ** carDnaDigits;
+
+  struct Car {
+    string name;
+    uint256 carDna;
   }
 
-  function updateMessage(string _newMessage) public {
-    message = _newMessage;
+  Car[] public cars;
+
+  event NewCar(uint256 carId, string name, uint256 carDna);
+
+  mapping(uint256 => address) public carToOwner;
+  mapping(address => uint256) public ownerCarCount;
+
+  function createCar(string _name) public onlyOwner {
+    uint256 _carDna = _generateRandomCarDna(_name);
+    uint256 id = cars.push(Car(_name, _carDna));
+
+    carToOwner[id] = msg.sender;
+    ownerCarCount[msg.sender] = ownerCarCount[msg.sender].add(1);
+
+    NewCar(id, _name, _carDna);
+  }
+
+  function _generateRandomCarDna(string _string) private view returns (uint256) {
+    uint rand = uint(keccak256(_string));
+
+    return rand % carDnaModulus;
+  }
+
+  function carsLength() public view returns (uint256) {
+    return cars.length;
   }
 }
